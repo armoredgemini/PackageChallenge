d253c0302be2c23d54e410c9928a810428314f2b
Archuleta <ac003588@ent.ad.dg.local>
2018-01-21 18:57:33 -0700

    Initial commit

---
+++ src/com/example/test/Main.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import com.tarchuleta.mylibrary.Series;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for(int i=0; i<10; i++){
+            System.out.println(Series.nFibonacci(i));
+        }
+    }
+}
